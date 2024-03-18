f3c6d2f6f498b2e1c0ec8206a825db965023e5a1
matheuss.fernandes <matheusfer33@hotmail.com>
2024-03-18 17:52:38 -0300

    First Commit

---
+++ tests/recipe_test.py
@@ -0,0 +1,25 @@
+import pytest
+from hamcrest import assert_that, equal_to
+from recipe_manager.recipe import Recipe
+
+
+def test_create_recipe_function():
+    recipe_dict = {"title": "apple pie",
+                   "description": "A basic american apple pie",
+                   "instructions": "BIG STRING",
+                   "category": "pie",
+                   "ingredients": [{"ingredient": "apple",
+                                    "quantity": "3_gr"},
+                                   {"ingredient": "flour",
+                                    "quantity": "350_gr"},
+                                   {"ingredient": "milk",
+                                    "quantity": "500_gr"},
+                                   {"ingredient": "eggs",
+                                    "quantity": "4_gr"}]}
+    recipe_obj = Recipe(recipe_dict["title"], recipe_dict["description"], recipe_dict["instructions"],
+                        recipe_dict["category"], recipe_dict["ingredients"])
+    assert_that(recipe_obj.create_new_recipe(), equal_to({"recipe_title": "apple pie",
+                                                          "recipe_description": "A basic american apple pie",
+                                                          "recipe_instructions": "BIG STRING",
+                                                          "recipe_category": "pie",
+                                                          "ingredients": recipe_obj.ingredients}))
