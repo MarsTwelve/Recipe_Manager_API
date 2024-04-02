4bbc688ce4f89f4eba315bf8e1a8663b841784b6
matheuss.fernandes <matheusfer33@hotmail.com>
2024-04-02 16:05:38 -0300

    Added docstrings to increase method understandability

--- recipe_manager/recipe.py
+++ recipe_manager/recipe.py
@@ -1,5 +1,48 @@
 class Recipe:
+    """
+    A class to represent a Recipe Object.
+    ...
+
+    Attributes
+    ----------
+    title: str
+        contains the recipe title
+    description: str
+        short description of the given recipe
+    instructions: str
+        contains instructions on how the recipe should be made
+    category: str
+        represents the food category of the recipe
+    ingredients: list
+        contains a list composed of small python dicts, storing the ingredients of the given recipe
+        Eg: {"ingredient": flour,
+            "quantity": 5_oz}
+
+    Methods
+    ---------
+    @property
+    show_recipe()
+        Returns a dict object that contains all parameters of the current instantiated recipe
+
+    add_ingredients(ingredients)
+        Receives a list of python dictionaries, containing the ingredients and quantities of a given recipe,
+        then it constructs multiple Ingredient objects and stores it within its own instance.
+    """
     def __init__(self, title, description, instructions, category):
+        """
+        Constructs all the necessary objects to make a Recipe object.
+
+        Parameters
+        -----------
+        :param title: str
+            contains the title of the recipe
+        :param description: str
+            contains a brief description of the recipe
+        :param instructions: str
+            contains a set of instructions on how the recipe should be made
+        :param category: str
+            contains the food group category the recipe falls into
+        """
         self.title = title
         self.description = description
         self.instructions = instructions
@@ -8,6 +51,15 @@ class Recipe:
 
     @property
     def show_recipe(self):
+        """
+        Returns a python dict of the given recipe
+        :return:
+            returns a python dict of the given recipe object
+            Eg: {"recipe_title": self.title,
+                "recipe_description": self.description,
+                "recipe_instructions": self.instructions,
+                "recipe_category": self.category}
+        """
         recipe = {"recipe_title": self.title,
                   "recipe_description": self.description,
                   "recipe_instructions": self.instructions,
@@ -15,11 +67,33 @@ class Recipe:
         return recipe
 
     def add_ingredients(self, ingredients):
+        """
+        Adds the given ingredients list as Ingredient objects into the Recipe object
+        :param ingredients: list
+            A list containing the ingredients needed for the recipe, should be a list of dictionaries
+        :return:
+        None
+        """
         for ingredient in ingredients:
             self.ingredients.append(Ingredients(ingredient["ingredient"], ingredient["quantity"]))
 
 
 class Ingredients:
+    """
+    Represents a specific ingredient and its quantity
+    ...
+    Attributes
+    ----------
+    ingredient: str
+        contains the ingredient name
+    quantity: str
+        contains the quantity of the given ingredient,
+        and also the unit used to measure said ingredient (ounces/oz, pounds/lbs or grams/g)
+
+    Methods
+    -------
+
+    """
     def __init__(self, ingredient, quantity):
         self.ingredient = ingredient
         self.quantity = quantity
