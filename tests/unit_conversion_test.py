37c8e2406c83b36c26ec6bb04fbd0cf2dbf28962
matheuss.fernandes <matheusfer33@hotmail.com>
2024-04-05 17:59:53 -0300

    refactored all tests to comply with better fixtures implemented in conftest.py

--- tests/unit_conversion_test.py
+++ tests/unit_conversion_test.py
@@ -2,42 +2,41 @@ from hamcrest import assert_that, equal_to
 from recipe_manager.unit_conversion import UnitConverter
 
 
-
-def test_unit_converter_parser_oz(create_instance_of_object_recipe_in_oz):
-    unit_converter_obj = UnitConverter(create_instance_of_object_recipe_in_oz.ingredients)
-    for ingredient in unit_converter_obj.ingredients_list:
-        unit_converter_obj.parse_ingredients_list(ingredient["quantity"])
-    assert_that(unit_converter_obj.unit, equal_to(["oz", "oz", "oz", "oz"]))
-
-
-def test_unit_converter_parser_lb(create_instance_of_object_recipe_in_lbs):
-    unit_converter_obj = UnitConverter(create_instance_of_object_recipe_in_lbs.ingredients)
+def test_unit_converter_parser_lb(create_recipe_object_using_lbs):
+    unit_converter_obj = UnitConverter(create_recipe_object_using_lbs.ingredients)
     for ingredient in unit_converter_obj.ingredients_list:
         unit_converter_obj.parse_ingredients_list(ingredient["quantity"])
     assert_that(unit_converter_obj.unit, equal_to(["lb", "lb", "lb", "lb"]))
 
 
-def test_unit_converter_parser_unit_int(create_instance_of_object_recipe_in_lbs):
-    unit_converter_obj = UnitConverter(create_instance_of_object_recipe_in_lbs.ingredients)
+def test_unit_converter_convert_unit_from_lb_to_g(create_recipe_object_using_lbs):
+    unit_converter_obj = UnitConverter(create_recipe_object_using_lbs.ingredients)
+    unit_converter_obj.get_unit()
+    assert_that(unit_converter_obj.convert_pounds_to_grams, equal_to([454, 249, 249, 363]))
+
+
+def test_unit_converter_parser_unit_int(create_recipe_object_using_lbs):
+    unit_converter_obj = UnitConverter(create_recipe_object_using_lbs.ingredients)
     for ingredient in unit_converter_obj.ingredients_list:
         unit_converter_obj.parse_ingredients_list(ingredient["quantity"])
     assert_that(unit_converter_obj.quantity, equal_to([1, 0.55, 0.55, 0.8]))
 
 
-def test_unit_converter_parser_unit_float(create_instance_of_object_recipe_in_oz):
-    unit_converter_obj = UnitConverter(create_instance_of_object_recipe_in_oz.ingredients)
+def test_unit_converter_parser_oz(create_recipe_object_using_oz):
+    unit_converter_obj = UnitConverter(create_recipe_object_using_oz.ingredients)
+    for ingredient in unit_converter_obj.ingredients_list:
+        unit_converter_obj.parse_ingredients_list(ingredient["quantity"])
+    assert_that(unit_converter_obj.unit, equal_to(["oz", "oz", "oz", "oz"]))
+
+
+def test_unit_converter_parser_unit_float(create_recipe_object_using_oz):
+    unit_converter_obj = UnitConverter(create_recipe_object_using_oz.ingredients)
     for ingredient in unit_converter_obj.ingredients_list:
         unit_converter_obj.parse_ingredients_list(ingredient["quantity"])
     assert_that(unit_converter_obj.quantity, equal_to([17.6, 8.8, 8.8, 14]))
 
 
-def test_unit_converter_convert_unit_from_lb_to_g(create_instance_of_object_recipe_in_lbs):
-    unit_converter_obj = UnitConverter(create_instance_of_object_recipe_in_lbs.ingredients)
-    unit_converter_obj.get_unit()
-    assert_that(unit_converter_obj.convert_pounds_to_grams, equal_to([454, 249, 249, 363]))
-
-
-def test_unit_converter_convert_unit_from_oz_to_g(create_instance_of_object_recipe_in_oz):
-    unit_converter_obj = UnitConverter(create_instance_of_object_recipe_in_oz.ingredients)
+def test_unit_converter_convert_unit_from_oz_to_g(create_recipe_object_using_oz):
+    unit_converter_obj = UnitConverter(create_recipe_object_using_oz.ingredients)
     unit_converter_obj.get_unit()
     assert_that(unit_converter_obj.convert_ounces_to_grams, equal_to([499, 249, 249, 397]))
